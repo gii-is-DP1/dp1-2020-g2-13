2263d4491e6438d84fbcda5c1a370b1237f6cf34
Isabel Arrans Vega <67335367+iarrans@users.noreply.github.com>
2020-11-21 12:33:17 +0100

    examen (#72)
    * Me llevo los dtasos a mi ramaza (#68)
    
    * Clase examen + creador del examen
    (falta la clase preguntas)

--- src/main/java/org/springframework/samples/petclinic/model/Examen.java
+++ src/main/java/org/springframework/samples/petclinic/model/Examen.java
@@ -1,6 +1,7 @@
 package org.springframework.samples.petclinic.model;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -11,6 +12,9 @@ import lombok.Data;
 public class Examen extends BaseEntity{
 
 	private String titulos;
-	private String puntuacionMaxima;
-	private String puntuacionMinima;
+	private Double puntuacionMaxima;
+	private Double puntuacionMinima;
+	
+	@ManyToOne(optional = false)
+	private Usuario usuario;
 }
