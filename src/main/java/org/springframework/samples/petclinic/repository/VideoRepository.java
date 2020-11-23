fa294c271ad85906dd226a8d9790484bd763baad
Isabel Arrans Vega <67335367+iarrans@users.noreply.github.com>
2020-11-23 13:51:08 +0100

    Entidad video (#100)

---
+++ src/main/java/org/springframework/samples/petclinic/repository/VideoRepository.java
@@ -0,0 +1,19 @@
+package org.springframework.samples.petclinic.repository;
+
+import java.util.Collection;
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.samples.petclinic.model.Hilo;
+import org.springframework.samples.petclinic.model.Video;
+
+
+public interface VideoRepository extends CrudRepository<Video, Integer> {
+	
+	Collection<Video> findAll();
+	
+	Optional<Video> findById(int id);
+	
+	void delete(Video video);
+
+}
---
+++ src/main/java/org/springframework/samples/petclinic/service/VideoService.java
@@ -0,0 +1,35 @@
+package org.springframework.samples.petclinic.service;
+
+import java.util.Collection;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.model.Pdf;
+import org.springframework.samples.petclinic.model.Video;
+import org.springframework.samples.petclinic.repository.VideoRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VideoService {
+	
+	@Autowired
+	VideoRepository videoRepository;
+	
+	public Collection<Video> findAll() {
+		return videoRepository.findAll();
+	}
+	
+	public Optional<Video> findById(int id) {
+		return videoRepository.findById(id);
+	}
+	
+	public void delete(Video video) {
+		videoRepository.deleteById(video.getId());
+
+	}
+	
+	public void save(Video video) {
+		videoRepository.save(video);
+	}
+
+}
