498e56f005e674b892d6924df47a6820405dce7d
Fran <fjbelrab@gmail.com>
2020-11-28 01:20:38 +0100

    2 tests añadidos

--- src/test/java/org/springframework/samples/petclinic/service/UsuarioServiceTests.java
+++ src/test/java/org/springframework/samples/petclinic/service/UsuarioServiceTests.java
@@ -1,20 +1,24 @@
 package org.springframework.samples.petclinic.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.BDDMockito.given;
 
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.samples.petclinic.model.Owner;
 import org.springframework.samples.petclinic.model.Pet;
 import org.springframework.samples.petclinic.model.Usuario;
+import org.springframework.samples.petclinic.model.businessrulesexceptions.ImpossibleUsuarioException;
+import org.springframework.transaction.annotation.Transactional;
 
 public class UsuarioServiceTests {
 	
-	private static final int TEST_USUARIO_ID = 27;
+	private static final int TEST_USUARIO_ID = 1;
 	
 	@Autowired
 	protected UsuarioService usuarioService;
@@ -41,6 +45,30 @@ public class UsuarioServiceTests {
 		Usuario usuario = this.usuarioService.findById(TEST_USUARIO_ID).get();
 		assertThat(usuario.getNombre()).startsWith("Guille");
 		assertThat(usuario.getApellidos()).isEqualTo("López");
+
+	}
+	@DisplayName("Prueba de localización de usuario")
+	@Test
+	void shouldFindById() {
+		Usuario usuario = this.usuarioService.findById(TEST_USUARIO_ID).get();
+		System.out.println(usuario.getNombre());
+		assertEquals("María José", usuario.getNombre());
+		assertEquals("Lera", usuario.getApellidos());
+
+	}
+	@DisplayName("Prueba de guardado de usuario")
+	@Test
+	void shouldSave() throws ImpossibleUsuarioException {
+		Usuario usuario = new Usuario();
+		usuario.setNombre("Fran");
+		usuario.setApellidos("Bel");
+		usuario.setLocalidad("El piso");
+		usuario.setColegio("La etsii");
+		usuario.setEmail("99999999999");
+		this.usuarioService.save(usuario);
+		assertThat(usuario.getId().longValue()).isNotEqualTo(0);
+		assertEquals("Fran", this.usuarioService.findById(usuario.getId()));
+		
 
 	}
 
