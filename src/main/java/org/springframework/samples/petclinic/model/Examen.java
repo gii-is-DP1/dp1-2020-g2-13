c5d3bbc55f5ab20cf61c42b0d361dcd53d55d2ef
Isabel Arrans Vega <67335367+iarrans@users.noreply.github.com>
2020-11-28 17:18:17 +0100

    valid (#126)

--- src/main/java/org/springframework/samples/petclinic/model/Examen.java
+++ src/main/java/org/springframework/samples/petclinic/model/Examen.java
@@ -3,6 +3,7 @@ package org.springframework.samples.petclinic.model;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 import lombok.Data;
 
@@ -11,8 +12,11 @@ import lombok.Data;
 @Table(name = "examenes")
 public class Examen extends BaseEntity{
 
+	@NotNull
 	private String titulos;
+	@NotNull
 	private Double puntuacionMaxima;
+	@NotNull
 	private Double puntuacionMinima;
 	
 	@ManyToOne(optional = false)
