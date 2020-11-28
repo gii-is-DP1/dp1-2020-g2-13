8f094f9f0f5228d370aef98ba01c1339c2d6e139
xamte <xamtehwt@gmail.com>
2020-11-28 13:16:27 +0100

    Relaciones n:n
    Usuarios-Logros
    Usuairos-Exámenes
    Usuarios-Hilos

--- src/main/java/org/springframework/samples/petclinic/model/Hilo.java
+++ src/main/java/org/springframework/samples/petclinic/model/Hilo.java
@@ -1,8 +1,12 @@
 package org.springframework.samples.petclinic.model;
 
+import java.util.Set;
+
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
 
 import lombok.Data;
 
@@ -23,4 +27,8 @@ public class Hilo extends BaseEntity {
 	
 	@ManyToOne(optional = false)
 	private Usuario usuario;
+
+	@NotEmpty
+	@ManyToMany
+	Set<Usuario> suscriptores;
 }
