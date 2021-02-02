aed06284f2fbb213220d3f55ef782dc780f5b020
guill <guill@DESKTOP-14BENS8>
2021-02-02 17:44:52 +0100

    funcionalidad de logros

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -35,8 +35,9 @@ INSERT INTO types VALUES (5,1,'bird');
 INSERT INTO types VALUES (6,1,'hamster');
 
 -- Insertar logros
-INSERT INTO logros(id,version, nombre,descripcion) VALUES (1,1, 'Hola_mundo', 'primer logro conseguido por registro');
-INSERT INTO logros(id,version, nombre,descripcion) VALUES (2,1, 'Chocapi', 'Choque con pizarra');
+INSERT INTO logros(id, nombre,descripcion) VALUES (1, 'Creaste un hilo', 'Logro que se consigue al crear tu primer hilo.');
+INSERT INTO logros(id, nombre,descripcion) VALUES (2, 'Hiciste un examen', 'Logro que se consigue al realizar un examen por primera vez.');
+INSERT INTO logros(id, nombre,descripcion) VALUES (3, 'Primer comentario', 'Logro que se consigue al comentar en un hilo por primera vez.');
 
 -- Insertar usuarios
 INSERT INTO usuarios(id, version, nombre,apellidos,localidad,colegio,email,username) VALUES (1,1, 'María José','Lera','Sevilla','Colegio de prueba','email@us.es','mjLera');
@@ -54,8 +55,9 @@ INSERT INTO videos(id,version, usuario_id, nombre, link, duracion) VALUES (1,1,1
 INSERT INTO examenes(id,version, titulos, puntuacion_maxima, puntuacion_minima, usuario_id) VALUES (1,1, 'prueba', 10, 0, 1);
 
 -- Insertar usuarios_logros
-INSERT INTO usuarios_logros(usuario_id, logros_id) VALUES (1, 1);
-INSERT INTO usuarios_logros(usuario_id, logros_id) VALUES (1, 2);
+INSERT INTO usuarios_logros(usuario_id, logros_id) VALUES (2, 1);
+INSERT INTO usuarios_logros(usuario_id, logros_id) VALUES (2, 2);
+INSERT INTO usuarios_logros(usuario_id, logros_id) VALUES (2, 3);
 
 -- Insertar usuarios_examenes
 --INSERT INTO usuarios_examenes(usuario_id, examenes_id) VALUES (2, 1);
