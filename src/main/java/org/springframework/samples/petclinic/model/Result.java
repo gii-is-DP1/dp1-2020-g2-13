83f4ddd5d0b63b09f648b936ab7b1ad7e6a15e1c
Fran <fjbelrab@gmail.com>
2021-02-06 13:06:04 +0100

    Consumir la api limeSurvey además de casi parsear el json a java objects

---
+++ src/main/java/org/springframework/samples/petclinic/model/Result.java
@@ -0,0 +1,65 @@
+
+package org.springframework.samples.petclinic.model;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Result {
+
+    private String sid;
+    private String surveylsTitle;
+    private Object startdate;
+    private Object expires;
+    private String active;
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    public String getSid() {
+        return sid;
+    }
+
+    public void setSid(String sid) {
+        this.sid = sid;
+    }
+
+    public String getSurveylsTitle() {
+        return surveylsTitle;
+    }
+
+    public void setSurveylsTitle(String surveylsTitle) {
+        this.surveylsTitle = surveylsTitle;
+    }
+
+    public Object getStartdate() {
+        return startdate;
+    }
+
+    public void setStartdate(Object startdate) {
+        this.startdate = startdate;
+    }
+
+    public Object getExpires() {
+        return expires;
+    }
+
+    public void setExpires(Object expires) {
+        this.expires = expires;
+    }
+
+    public String getActive() {
+        return active;
+    }
+
+    public void setActive(String active) {
+        this.active = active;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
